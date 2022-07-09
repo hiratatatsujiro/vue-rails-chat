7e398d71899cb3d04aecb8ea098cc64b7d399182
平田達二郎 <hiratatoorujirou@hiratatoorujirounoMacBook-Pro.local>
2022-07-10 05:38:41 +0900

    firstcommit

---
+++ live-chat-vuejs/src/components/TestComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    これはコンポーネントから表示されています。
+    <p>{{ message }}</p>
+    <button @click="onClick">親メソッド実行</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message', 'isEnabled'],
+  methods: {
+    onClick () {
+      this.$emit("toggle")
+    }
+  }
+}
+</script>
+
